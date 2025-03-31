8e7a2c7e9d1c85ae8a8aac8d2f5ed3fff156e6f8
Dipesh <dipeshgavit333@gmail.com>
2025-04-01 00:53:40 +0530

    updated cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:4000', 'http://localhost:5500', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'https://ainotesapp.netlify.app', 'http://127.0.0.1:3000'],
   credentials: true
 }));
 app.use(express.json());
